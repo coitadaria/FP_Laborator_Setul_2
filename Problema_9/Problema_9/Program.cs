12f35a489348041f1aaac85996d59751622da523
Coita Daria <dariacoita95@gmail.com>
2025-11-30 15:11:16 +0200

    Add files via upload

---
+++ Problema_9/Problema_9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Programul verifica daca o secventa de n numere este monotona.");
+        int n;
+        Console.WriteLine("Introduceti numarul de elemente n: ");
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.Write("Introduceti un numar natural valid (n > 0): ");
+        }
+
+        bool esteCrescatoare = true;
+        bool esteDescrescatoare = true;
+        int ?previousNumber = null;
+
+        Console.WriteLine($"Introduceti cele {n} numere:");
+
+        for (int i = 0; i < n; i++)
+        {
+            int numar;
+            Console.Write($"Numarul {i + 1}: ");
+            while (!int.TryParse(Console.ReadLine(), out numar))
+            {
+                Console.Write("Introduceti un numar valid: ");
+            }
+
+            if(previousNumber.HasValue)
+            {
+                if (numar < previousNumber.Value)
+                {
+                    esteCrescatoare = false;
+                }
+
+                if (numar > previousNumber.Value)
+                {
+                    esteDescrescatoare = false;
+                }
+            }
+            previousNumber = numar;
+        }
+
+        if (esteCrescatoare)
+        {
+            Console.WriteLine("Secventa este monoton crescatoare.");
+        }
+        else
+        {
+            if (esteDescrescatoare)
+            {
+                Console.WriteLine("Secventa este monoton descrescatoare.");
+            }
+            else
+            {
+                Console.WriteLine("Secventa nu este monotona.");
+            }
+        }
+
+    }
+}
